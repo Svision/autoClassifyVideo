a08443134d7230f29758307846c901389d8e498f
Changhao Song <changhao.song@mail.utoronto.ca>
2021-12-05 17:05:58 -0500

    Update classify_videos.sh

--- classify_videos.sh
+++ classify_videos.sh
@@ -4,15 +4,15 @@ for dir in /volume1/downloads/*/ ; do
 	:
   elif [[ $dir == *"S0"* ]] || [[ $dir == *"S1"* ]]; then # tv show has session number
     full="${dir%"${dir##*[!/]}"}"
-    full="${full##*/}"
+    full="${full##*/}" # get name without absolute path
     searchstring="S0"
     rest=${full#*$searchstring}
     name_index=$(( ${#full} - ${#rest} - ${#searchstring} )) # before session number
     name=${full:0:$((name_index-1))}
-    name=${name//./ }
+    name=${name//./ } # replace . to <space>
     
     mv_to="/volume1/video/TV Shows/${name}"
-    if [ ! -d "$mv_to" ]; then
+    if [ ! -d "$mv_to" ]; then # check if path exist
       mkdir "$mv_to"
     fi
     mv $dir "$mv_to"
